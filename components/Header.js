43b945fb852bdcd46024bec0098dfe0f30baaf3e
Siddhesh Kankekar <kankekas@tcd.ie>
2021-08-05 14:50:57 +0100

    header component-start

--- components/Header.js
+++ components/Header.js
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 function Header() {
   return (
-    <header>
-      <div className="relative">
+    <header className="sticky top-0 z-50">
+      <div className="relative flex items-center h-10 cursor-pointer my-auto">
         <Image
           src="https:links.papareact.com/qd3"
           layout="fill"
