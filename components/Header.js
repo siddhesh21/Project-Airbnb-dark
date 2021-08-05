6778941202044661f9798e97ddf9b306d219ea2b
Siddhesh Kankekar <kankekas@tcd.ie>
2021-08-05 16:30:39 +0100

    Added DARK theme + completed Header + Banner component

--- components/Header.js
+++ components/Header.js
@@ -1,9 +1,17 @@
 import Image from "next/image";
+import { SearchIcon } from "@heroicons/react/solid";
+import { GlobeAltIcon } from "@heroicons/react/outline";
+import { MenuIcon } from "@heroicons/react/outline";
+import { UserCircleIcon } from "@heroicons/react/solid";
 
 function Header() {
   return (
-    <header className="sticky top-0 z-50">
-      <div className="relative flex items-center h-10 cursor-pointer my-auto">
+    <header
+      className="sticky top-0 z-50 grid grid-cols-3 bg-black-700 shadow-xl p-5
+    md:px-10"
+    >
+      {/* LEFT */}
+      <div className="relative flex items-center h-10 cursor-pointer my-auto ">
         <Image
           src="https:links.papareact.com/qd3"
           layout="fill"
@@ -11,8 +19,30 @@ function Header() {
           objectPosition="left"
         />
       </div>
-      <div className=""></div>
-      <div className=""></div>
+
+      {/* MIDDLE */}
+      <div className="flex items-center md:border-2 rounded-full py-2 md:shadow-lg">
+        <input
+          className="flex-grow pl-5 bg-transparent outline-none"
+          type="text"
+          placeholder="Search"
+        />
+        <SearchIcon
+          className="hidden md:inline-flex h-8 bg-red-400 text-white 
+          rounded-full p-2 cursor-pointer md:mx-2"
+        />
+      </div>
+
+      {/* RIGHT */}
+      <div className="flex items-center space-x-4 justify-end">
+        <p className="hidden md:inline ">Become a HOST</p>
+        <GlobeAltIcon className="h-6 cursor-pointer" />
+
+        <div className="flex items-center space-x-2 border-2 p-2 rounded-full">
+          <MenuIcon className="h-6" />
+          <UserCircleIcon className="h-6" />
+        </div>
+      </div>
     </header>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ import Banner from "../components/Banner";
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+    <div>
       <Head>
         <title>Airbnb</title>
         <link rel="icon" href="/favicon.ico" />
