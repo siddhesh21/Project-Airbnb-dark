b46698aa2956f0838724dab7c240b2661dd3b0d4
Siddhesh Kankekar <kankekas@tcd.ie>
2021-08-06 15:29:42 +0100

    Added loading bar

--- components/InfoCard.js
+++ components/InfoCard.js
@@ -9,7 +9,12 @@ function InfoCard({ img, location, title, description, star, price, total }) {
     hover:shadow-sm transition duration-200 ease-out first:border-t"
     >
       <div className="relative h-24 w-40 md:h-52 md:w-88 md:w-80 flex-shrink-0">
-        <Image src={img} layout="fill" ObjectFit="cover" />
+        <Image
+          className="rounded-2xl"
+          src={img}
+          layout="fill"
+          ObjectFit="cover"
+        />
       </div>
 
       <div className="flex flex-col flex-grow pl-5">
