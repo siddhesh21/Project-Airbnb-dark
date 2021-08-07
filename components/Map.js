0795cac911e948ba2bbe0a0580e5c4c7601d0ea8
Siddhesh Kankekar <kankekas@tcd.ie>
2021-08-07 16:49:12 +0100

    added center for each locations

--- components/Map.js
+++ components/Map.js
@@ -1,11 +1,26 @@
 import ReactMapGL from "react-map-gl";
 import { useState } from "react";
-function Map() {
+import getCenter from "geolib/es/getCenter";
+
+function Map({ searchResults }) {
+  //   Transforming the searchResults DEMO API object into geolib required format
+
+  const coordinates = searchResults.map((result) => ({
+    longitude: result.long,
+    latitude: result.lat,
+  }));
+
+  {
+    /**console.log(coordinates); */
+  }
+
+  const center = getCenter(coordinates);
+
   const [viewport, setViewport] = useState({
     width: "100%",
     height: "100%",
-    latitude: 37.7577,
-    longitude: -122.4376,
+    latitude: center.latitude,
+    longitude: center.longitude,
     zoom: 8,
   });
   return (
